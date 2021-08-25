17c466cdc063d2e78ddc350fe4c9fadd39f11d78
awkonecki <awkonecki.developer@gmail.com>
2021-08-24 21:53:04 -0700

    DB Schema Migration Support

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 import sys
 
@@ -9,15 +10,18 @@ db = SQLAlchemy(app,
 # session_options={"expire_on_commit": False} # provides a means that after a commit data in the session is invalid.
 )
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-
+    completed = db.Column(db.Boolean(), nullable=False, default=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.decription}>'
 
-db.create_all()
+# migration will take care of the creation of the database.
+# db.create_all()
 
 def create_todo_entry(description):
     try:
